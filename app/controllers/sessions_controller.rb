ec0e2d8aef55427a917c0ee893134dd4486d1ae9
CaseyInHaengsin <casey.rowley89@gmail.com>
2020-03-31 05:26:26 -0600

    adjusted login functionality.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,7 +20,9 @@ class SessionsController < ApplicationController
     end
 
     def destroy
-
+        session[:user_id] = nil
+        flash[:success] = "you have successfully logged out"
+        redirect_to login_path
 
     end
 
